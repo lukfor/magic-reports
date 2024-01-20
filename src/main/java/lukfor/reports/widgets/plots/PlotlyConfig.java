6547e58b49321038ae5d9fdd46e115aeca748711
Lukas Forer <lukas.forer@i-med.ac.at>
2024-01-20 11:48:38 +0100

    Add bar and xy support to plotly

--- src/main/java/lukfor/reports/widgets/plots/PlotlyConfig.java
+++ src/main/java/lukfor/reports/widgets/plots/PlotlyConfig.java
@@ -34,6 +34,7 @@ public class PlotlyConfig implements IWidgetConfig {
         defaultParams.put("type", "scatter");
         defaultParams.put("mode", "markers");
         defaultParams.putAll(params);
+        parseXY(defaultParams);
         traces.add(defaultParams);
     }
 
@@ -51,11 +52,44 @@ public class PlotlyConfig implements IWidgetConfig {
         traces.add(defaultParams);
     }
 
+    public void bar(HashMap<String, Object> params){
+        HashMap<String, Object> defaultParams = new HashMap<String, Object>();
+        defaultParams.put("type", "bar");
+        defaultParams.putAll(params);
+        parseXY(defaultParams);
+        traces.add(defaultParams);
+    }
     public List<HashMap<String, Object>> getTraces() {
         return traces;
     }
 
     public Map<String, Object> getLayout() {
         return layout;
+    }
+
+    private void parseXY(HashMap<String, Object> params) {
+        Object data = params.get("xy");
+        if (data != null){
+            if (data instanceof  Map){
+                Map map = (Map) data;
+                List x = new Vector();
+                List y = new Vector();
+                for (Object key: map.keySet()){
+                    x.add(key);
+                    y.add(map.get(key));
+                }
+                if (params.containsKey("x")){
+                    throw new RuntimeException("property xy not supported. x already set.");
+                }
+                params.put("x", x);
+                if (params.containsKey("y")){
+                    throw new RuntimeException("property xy not supported. y already set.");
+                }
+                params.put("y", y);
+                params.remove("xy");
+            } else {
+                throw new RuntimeException("property xy doesn't support object of type " + data.getClass());
+            }
+        }
     }
 }
